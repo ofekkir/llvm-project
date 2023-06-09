7949143be27dc59de806803465f7abaf1cea9edc
ofek kirzner <ofekkir@gmail.com>
2023-06-09 18:57:07 +0300

    Change tool action to plugin action

--- clang-tools-extra/loop-convert/LoopConvert.cpp
+++ clang-tools-extra/loop-convert/LoopConvert.cpp
@@ -7,10 +7,19 @@
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "llvm/Support/CommandLine.h"
 
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Sema/Sema.h"
+#include "llvm/Support/raw_ostream.h"
+
 using namespace clang::tooling;
 using namespace llvm;
 using namespace clang;
 using namespace clang::ast_matchers;
+namespace {
 
 auto LoopMatcher =
   recordDecl(
@@ -35,9 +44,6 @@ auto LoopMatcher =
     )
   ).bind("external_container");
 
-// auto LoopMatcher =
-// pointerType(pointee(hasMemberName("koko"))).bind("pointer");
-// auto LoopMatcher = fieldDecl(hasType(pointerType())).bind("pointer");
 
 class LoopPrinter : public MatchFinder::MatchCallback {
 public:
@@ -48,32 +54,55 @@ public:
   }
 };
 
-// Apply a custom category to all command-line options so that they are the
-// only ones displayed.
-static llvm::cl::OptionCategory MyToolCategory("my-tool options");
 
-// CommonOptionsParser declares HelpMessage with a description of the common
-// command-line options related to the compilation database and input files.
-// It's nice to have this help message in all tools.
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 
-// A help message for this specific tool can be added afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...\n");
-
-int main(int argc, const char **argv) {
-  auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory);
-  if (!ExpectedParser) {
-    // Fail gracefully for unsupported options.
-    llvm::errs() << ExpectedParser.takeError();
-    return 1;
+class PrintFunctionNamesAction : public PluginASTAction {
+  std::set<std::string> ParsedTemplates;
+protected:
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
+                                                 llvm::StringRef) override {
+    Finder.addMatcher(LoopMatcher, &Printer);
+    return Finder.newASTConsumer();
   }
-  CommonOptionsParser &OptionsParser = ExpectedParser.get();
-  ClangTool Tool(OptionsParser.getCompilations(),
-                 OptionsParser.getSourcePathList());
 
+  bool ParseArgs(const CompilerInstance &CI,
+                 const std::vector<std::string> &args) override {
+    for (unsigned i = 0, e = args.size(); i != e; ++i) {
+      llvm::errs() << "PrintFunctionNames arg = " << args[i] << "\n";
+
+      // Example error handling.
+      DiagnosticsEngine &D = CI.getDiagnostics();
+      if (args[i] == "-an-error") {
+        unsigned DiagID = D.getCustomDiagID(DiagnosticsEngine::Error,
+                                            "invalid argument '%0'");
+        D.Report(DiagID) << args[i];
+        return false;
+      } else if (args[i] == "-parse-template") {
+        if (i + 1 >= e) {
+          D.Report(D.getCustomDiagID(DiagnosticsEngine::Error,
+                                     "missing -parse-template argument"));
+          return false;
+        }
+        ++i;
+        ParsedTemplates.insert(args[i]);
+      }
+    }
+    if (!args.empty() && args[0] == "help")
+      PrintHelp(llvm::errs());
+
+    return true;
+  }
+  void PrintHelp(llvm::raw_ostream& ros) {
+    ros << "Help for PrintFunctionNames plugin goes here\n";
+  }
+
+private:
   LoopPrinter Printer;
   MatchFinder Finder;
-  Finder.addMatcher(LoopMatcher, &Printer);
 
-  return Tool.run(newFrontendActionFactory(&Finder).get());
+};
+
 }
+
+static FrontendPluginRegistry::Add<PrintFunctionNamesAction>
+X("print-fns", "print function names");
